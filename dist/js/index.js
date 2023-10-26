3505458deb1f8501a051e466c4b81b4dcd347ccb
ABojo <Alex.bojorquez2598@gmail.com>
2023-10-26 19:56:51 -0400

    Made mobile nav and social links accessible
    I added text for screenreaders to both the mobile nav toggle button and the links
    to my social media. Lastly, I added aria-expanded and aria-controls to the mobile
    nav button.

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,4 +3,5 @@ const navToggle = document.querySelector(".header__nav-toggle");
 
 navToggle.addEventListener("click", () => {
   navigation.classList.toggle("header__nav--open");
+  navToggle.setAttribute("aria-expanded", navToggle.getAttribute("aria-expanded") === "true" ? false : true);
 });
