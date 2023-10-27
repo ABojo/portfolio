10d2a5d067e6f09bde2faa0ed3410a7a43e63c41
ABojo <Alex.bojorquez2598@gmail.com>
2023-10-26 22:52:04 -0400

    Added code that will update the active nav link as the user scrolls
    I wanted the active nav link to change as the user scrolls between sections, so I
    added JS that will keep the nav in sync with the current position on the page.

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,35 @@
 const navigation = document.querySelector(".header__nav");
 const navToggle = document.querySelector(".header__nav-toggle");
+const navLinks = document.querySelectorAll(".header__link");
+const sections = document.querySelectorAll("section");
 
 navToggle.addEventListener("click", () => {
   navigation.classList.toggle("header__nav--open");
   navToggle.setAttribute("aria-expanded", navToggle.getAttribute("aria-expanded") === "true" ? false : true);
+});
+
+function getCurrentSectionId() {
+  let lastSectionId = null;
+
+  for (let i = 0; i < sections.length; i++) {
+    if (window.scrollY >= sections[i].offsetTop) {
+      lastSectionId = sections[i].id;
+    }
+  }
+
+  return lastSectionId;
+}
+
+function syncNavStyling(activeId) {
+  navLinks.forEach((navLink) => {
+    if (navLink.href.includes(activeId)) {
+      navLink.classList.add("header__link--active");
+    } else {
+      navLink.classList.remove("header__link--active");
+    }
+  });
+}
+
+window.addEventListener("scroll", () => {
+  syncNavStyling(getCurrentSectionId());
 });
